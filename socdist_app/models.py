d2e74def85153b814e8b20bf6749d38935b3dc57
DRHood <darryl.r.hood@gmail.com>
2020-04-05 17:29:45 -0400

    app data models created

--- socdist_app/models.py
+++ socdist_app/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=60)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=480)
+    photo_url = models.CharField(max_length=240)
+    location = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
+    # password = TODO
+    # events = Event relationship: many to many
+    # groups = Group relationship: many to many
+
+    class Meta:
+        ordering = ['username']
+
+    def __str__(self):
+        return self.username
+
+class Group(models.Model):
+    # organizer = User relationship: many to one TODO
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    group_name = models.CharField(max_length=60)
+    group_description = models.CharField(max_length=240)
+    public = models.BooleanField(default=True)
+    # individual_invites = User relationship: many to many
+    individual_invites = models.ManyToManyField(User)
+    # members = User relationship: many to many
+    members = models.ManyToManyField(User)
+    # events = Event relationship: one to many
+
+    class Meta:
+        ordering = ['group_name']
+
+    def __str__(self):
+        return self.group_name
+
+class Event(models.Model):
+    # organizer = User or Group relationship: many to one
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    event_name = models.CharField(max_length=60)
+    event_description = models.CharField(max_length=240)
+    event_img_url = models.CharField(max_length=240)
+    event_date = models.DateField()
+    event_time = models.TimeField()
+    # group = Group relationship: many to one
+    group_invite = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='events')
+    # individual_invites = User relationship: many to many
+    individual_invites = models.ManyToManyField(User)
+    # members = User relatioship: many to many
+    members = models.ManyToManyField(User)
+    
+    class Meta:
+        ordering = ['event_name']
+
+    def __str__(self):
+        return self.event_name
+        
