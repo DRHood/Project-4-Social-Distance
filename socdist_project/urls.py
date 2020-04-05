6b64cf06393085c0ba818a600521608e7d4111ff
DRHood <darryl.r.hood@gmail.com>
2020-04-05 10:40:28 -0400

    updated urls

--- socdist_project/urls.py
+++ socdist_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.FrontendAppView.as_view()), #New URL for index route
+    path('api/v1/', include('tunr_app.urls')),
+    re_path(r'^.*$', views.FrontendAppView.as_view()), #Says search Frontend for all other routes, so refreshing doesn't make the deployed page error
 ]
