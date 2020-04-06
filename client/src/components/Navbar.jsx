19b390585c406bca26e0e9c7d4bac03384b71b37
DRHood <darryl.r.hood@gmail.com>
2020-04-06 18:54:03 -0400

    adding navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Soc_Dist</h1>
+                <div>
+                    <div><Link to="/">Home</Link></div>
+                </div>
+            </div>
+        )
+    }
+}
