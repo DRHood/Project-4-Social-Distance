a8dfd480425cf0e4fcc0b34f570d24470d62ff27
DRHood <darryl.r.hood@gmail.com>
2020-04-06 02:40:07 -0400

    app urls set

--- socdist_app/urls.py
+++ socdist_app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register('users', views.UserView)
+router.register('groups', views.GroupView)
+router.register('events', views.EventView)
+
 urlpatterns = [
-    
+    path('', include(router.urls))
 ]
