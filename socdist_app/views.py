abe3cf688537ed1addfa07017bb84701011dd12d
DRHood <darryl.r.hood@gmail.com>
2020-04-06 01:56:59 -0400

    app views set

--- socdist_app/views.py
+++ socdist_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, EventSerializer
+from .models import User, Group, Event
 
-# Create your views here.
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GroupView(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class EventView(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
